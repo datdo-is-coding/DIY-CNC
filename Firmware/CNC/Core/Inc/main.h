8dcb3433e3f1d510bfa2d28ca6b0cf5238335c5b
dat <dat30581@gmail.com>
2026-08-15 00:09:17 +0700

    Trajectory Block Queue Added

--- Firmware/CNC/Core/Inc/main.h
+++ Firmware/CNC/Core/Inc/main.h
@@ -28,7 +28,8 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_hal.h"
-
+#include "tim.h"
+#include "dma.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -48,8 +49,6 @@ extern "C" {
 /* USER CODE BEGIN EM */
 
 /* USER CODE END EM */
-
-void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
 
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
